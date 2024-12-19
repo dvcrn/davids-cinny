20df3e635fc81e176907f2fee09c7a6d590194c4
David Mohl <git@d.sh>
2024-12-19 22:24:44 +0900

    Add all rooms to Home, not just orphaned ones

--- src/app/pages/client/home/useHomeRooms.ts
+++ src/app/pages/client/home/useHomeRooms.ts
@@ -3,12 +3,14 @@ import { useMatrixClient } from '../../../hooks/useMatrixClient';
 import { mDirectAtom } from '../../../state/mDirectList';
 import { roomToParentsAtom } from '../../../state/room/roomToParents';
 import { allRoomsAtom } from '../../../state/room-list/roomList';
-import { useOrphanRooms } from '../../../state/hooks/roomList';
+import { useOrphanRooms, useRooms } from '../../../state/hooks/roomList';
 
 export const useHomeRooms = () => {
   const mx = useMatrixClient();
   const mDirects = useAtomValue(mDirectAtom);
   const roomToParents = useAtomValue(roomToParentsAtom);
-  const rooms = useOrphanRooms(mx, allRoomsAtom, mDirects, roomToParents);
+  // const rooms = useOrphanRooms(mx, allRoomsAtom, mDirects, roomToParents);
+  const rooms = useRooms(mx, allRoomsAtom, mDirects);
+
   return rooms;
 };
